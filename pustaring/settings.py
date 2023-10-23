e7dbf8db3be620700f539edad213fcf898c7b332
rhaken <rhakenss@gmail.com>
2023-10-23 18:13:09 +0700

    update setting.py

--- pustaring/settings.py
+++ pustaring/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'beranda',
+    'customer_service',
+    'fitur_premium',
+    'peminjaman_buku',
+    'sistem_manajemen',
+    'ulasan',
 ]
 
 MIDDLEWARE = [
