f210aa0751cdeb042ae79c27a9af9e3cf20c8545
davin42 <124948495+davin42@users.noreply.github.com>
2023-10-25 14:23:15 +0700

    Update apps.py

--- beranda/apps.py
+++ beranda/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class BerandaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'beranda'
+
+    def ready(self):
+        import beranda.signals  # Impor file 'signals.py' atau yang sesuai dari modul 'beranda'
