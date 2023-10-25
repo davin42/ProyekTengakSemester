a755c44f96ec425e575e72354a71860ab220e686
davin42 <124948495+davin42@users.noreply.github.com>
2023-10-25 14:38:55 +0700

    Update admin.py

--- beranda/admin.py
+++ beranda/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from .models import Book  # Gantilah dengan model buku Anda
+from .models import Book
 
-# Daftarkan model buku di admin
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date', 'genre')  # Atur bidang yang ingin ditampilkan di daftar admin
-    list_filter = ('genre', 'published_date')  # Filter data berdasarkan genre dan tanggal terbit
-    search_fields = ('title', 'author')  # Tambahkan bidang pencarian
-
-# Jika Anda ingin mengelola model lain dalam modul "Beranda," Anda juga bisa mendaftarkannya di sini.
+    list_display = ('title', 'author', 'published_date', 'genre')
+    list_filter = ('genre', 'published_date')
+    search_fields = ('title', 'author')
