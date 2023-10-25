28b2e0e1ab03d7aec7da3efd173b1acfe94141a9
davin42 <124948495+davin42@users.noreply.github.com>
2023-10-25 14:37:02 +0700

    Update views.py

--- beranda/views.py
+++ beranda/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Book  # Gantilah dengan model buku yang sesuai
 
-# Create your views here.
+def beranda(request):
+    # Ambil data buku dari basis data
+    books = Book.objects.all()  # Mengambil semua buku, Anda bisa menambahkan filter sesuai kebutuhan
+
+    # Implementasikan fitur filter di sini (gunakan request.GET untuk mendapatkan parameter filter)
+
+    # Render halaman beranda dengan data buku
+    return render(request, 'beranda.html', {'books': books})
